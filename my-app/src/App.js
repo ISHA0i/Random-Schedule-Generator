4c223687b6f5f167d71444bc9ccf0c2a283fe4f7
ISHA0i <ishachovatiya2203@gmail.com>
2025-05-29 01:23:28 +0530

    refactor: downgrade react-router-dom to version 6.11.0 and update import statements in App.js for better readability

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
 import Navbar from './Component/Navbar';
 import Input from './Component/Home/Input';
 import Output from './Component/Detail/Output';
